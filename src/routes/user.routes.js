d5a0d71c92563a7569b033fb8ee65bf4cb560bfc
DAbharat <bharat05jha@gmail.com>
2025-08-16 14:02:50 +0530

    authorization

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { registerUser, loginUser, logoutUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middlewares.js";
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
+
 
 const router = Router();
 
@@ -12,5 +14,7 @@ router.route("/register").post(
         }
     ]), registerUser
 )
+
+router.route("/logout").post(verifyJWT, logoutUser)
 
 export default router
